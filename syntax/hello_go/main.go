b8fda8351da7f6ce9692b6f456f396058b1b6195
wangxy <wangxuyang16@crdigital.com.cn>
2023-09-26 16:46:14 +0800

    hello go

--- syntax/hello_go/main.go
+++ syntax/hello_go/main.go
@@ -1,5 +1,45 @@
-package hello_go
+package main
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	println("Hello, Go!")
+	fmt.Println(Test3(4))
+}
+
+func Hello() {
+	list := []*Demo{{"a"}, {"b"}}
+	for _, item := range list {
+		go func(item *Demo) {
+			fmt.Println("name" + item.Name)
+		}(item)
+	}
+	time.Sleep(100 * time.Millisecond)
+}
+
+type Demo struct {
+	Name string
+}
+
+func Test1() {
+	a := 1
+	defer fmt.Println(a)
+}
+
+func Test2() int {
+	a := 1
+	defer func() {
+		a = 0
+	}()
+	return a
+}
+
+func Test3(a int) int {
+	defer func(a int) {
+		a = 0
+	}(a)
+	return a
 }
